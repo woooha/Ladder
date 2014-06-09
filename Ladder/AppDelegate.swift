8a8bc8fe70bdeda6cec0b98f089ff922e49fb5e2
yangfan12 <yangfan12@baidu.com>
2014-06-09 23:55:56 +0800

    Init

---
+++ Ladder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Ladder
+//
+//  Created by Fan Yang on 6/9/14.
+//  Copyright (c) 2014 woooha. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Ladder/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Ladder
+//
+//  Created by Fan Yang on 6/9/14.
+//  Copyright (c) 2014 woooha. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
